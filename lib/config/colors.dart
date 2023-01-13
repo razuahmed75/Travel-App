e3cc34abc7b032e380450df7a03123ca206d3dc5
Razu ahmed <99271821+razuahmed75@users.noreply.github.com>
2023-01-14 01:55:01 +0600

    update

--- lib/config/colors.dart
+++ lib/config/colors.dart
@@ -4,10 +4,12 @@ class AppColors {
   static final textColor = const Color(0xFF9C9C9C);
   static final iconColor = const Color(0xFFD0D0D0CC);
   static final scaffoldColor = const Color(0xFFF7F7F7);
-  static final fieldColor = const Color(0xFFF7F7F766);
-  static final yellowColor = const Color(0xFFF4B806);
   static final whiteColor = const Color(0xFFFFFFFF);
+
+  static final fieldColor = const Color(0xFFF7F7F766);
+  static final secondaryColor = const Color(0xFFCDFEE659);
+  static final yellowColor = const Color(0xFFF4B806);
+
   static final blackColor = const Color(0xFF131B1A);
   static final mainColor = const Color(0xFF08BA64);
-  static final secondaryColor = const Color(0xFFCDFEE659);
 }
